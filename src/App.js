19d65e36b53e90e56ec7eb3b0ba8354ee3155ad6
Mateo <mdavidrodriguez@unicesar.edu.co>
2022-12-28 14:53:02 -0500

    Finalizado

--- src/App.js
+++ src/App.js
@@ -1,57 +1,67 @@
 import { Component } from "react";
-import Productos from './components/Productos'
-import Layout from './components/Layout'
-import Title from './components/Title'
-import Navbar from './components/Navbar'
-
-
-
+import Productos from "./components/Productos";
+import Layout from "./components/Layout";
+import Title from "./components/Title";
+import Navbar from "./components/Navbar";
 
 class App extends Component {
-  state= {
+  state = {
     productos: [
-      {name: 'tomate', price:1500, img: '/productos/tomate.jpg'},
-      {name: 'Arbejas', price:2500, img: '/Productos/arbejas.jpg'},
-      {name: 'Lechuga', price:500, img: '/Productos/lechuga.jpg'}
+      { name: "tomate", price: 1500, img: "/productos/tomate.jpg" },
+      { name: "Arbejas", price: 2500, img: "/Productos/arbejas.jpg" },
+      { name: "Lechuga", price: 500, img: "/Productos/lechuga.jpg" },
     ],
-    carro: [
+    carro: [],
+    esCarroVisible: false,
+  };
 
-    ],
 
-  }
-agregarAlCarro= (producto) => {
-  const {carro} = this.state
-  if(carro.find(x => x.name === producto.name)){
-    const newCarro = carro.map(x => x.name === producto.name
-      ? ({
-        ...x, 
-        cantidad: x.cantidad + 1
-      })
-      : x )
-      return this.setState({carro: newCarro})
-  }
-  // console.log(producto);
-  return this.setState({
-    carro: this.state.carro.concat({
-      ...producto,
-      cantidad: 1,
-    })
-  })
-}
 
+  agregarAlCarro = (producto) => {
+    const { carro } = this.state;
+    if (carro.find((x) => x.name === producto.name)) {
+      const newCarro = carro.map((x) =>
+        x.name === producto.name
+          ? {
+              ...x,
+              cantidad: x.cantidad + 1,
+            }
+          : x
+      );
+      return this.setState({ carro: newCarro });
+    }
+    // console.log(producto);
+    return this.setState({
+      carro: this.state.carro.concat({
+        ...producto,
+        cantidad: 1,
+      }),
+    });
+  };
+
+  mostrarCarro = () => {
+    if(!this.state.carro.length) {
+      return
+    }
+    this.setState({ esCarroVisible: !this.state.esCarroVisible });
+  };
 
   render() {
+    const { esCarroVisible } = this.state;
     return (
       <div>
-        <Navbar carro={this.state.carro}/>
+        <Navbar
+          carro={this.state.carro}
+          esCarroVisible={esCarroVisible}
+          mostrarCarro={this.mostrarCarro}
+        />
         <Layout>
           <Title />
-        <Productos 
-          agregarAlCarro={this.agregarAlCarro}
-          productos={this.state.productos}
-        />
+          <Productos
+            agregarAlCarro={this.agregarAlCarro}
+            productos={this.state.productos}
+          />
         </Layout>
-        
       </div>
     );
   }
--- src/components/Carro.js
+++ src/components/Carro.js
@@ -1,7 +1,6 @@
 import { Component } from "react";
 import BubbleAlert from "./BubbleAlert";
-import DetallesCarro from './DetallesCarro'
-
+import DetallesCarro from "./DetallesCarro";
 
 const styles = {
   carro: {
@@ -20,15 +19,17 @@ const styles = {
 };
 class Carro extends Component {
   render() {
-    const { carro } = this.props;
-    const cantidad = carro.reduce((acc, el) => acc + el.cantidad,0);
+    const { carro, esCarroVisible, mostrarCarro } = this.props;
+    const cantidad = carro.reduce((acc, el) => acc + el.cantidad, 0);
     return (
       <div>
         <span style={styles.buble}>
           {cantidad !== 0 ? <BubbleAlert value={cantidad} /> : null}
         </span>
-        <button style={styles.carro}>Carro</button>
-        <DetallesCarro carro={carro}/>
+        <button onClick={mostrarCarro} style={styles.carro}>
+          Carro
+        </button>
+        {esCarroVisible ? <DetallesCarro carro={carro} /> : null}
       </div>
     );
   }
